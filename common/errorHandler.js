7ed57d73d916aaf4ee467cead2bd3487820e3819
Harshit Gupta <104277488+Harshit69P@users.noreply.github.com>
2024-01-28 15:14:42 +0530

    Update errorHandler.js

--- common/errorHandler.js
+++ common/errorHandler.js
@@ -1,6 +1,6 @@
-const errorFunction = (errorBit, msg, data) => {
-    if (errorBit) return { is_error: errorBit, message: msg };
-    else return { is_error: errorBit, message: msg, data };
-};
+// const errorFunction = (errorBit, msg, data) => {
+//     if (errorBit) return { is_error: errorBit, message: msg };
+//     else return { is_error: errorBit, message: msg, data };
+// };
 
-module.exports = errorFunction;
+// module.exports = errorFunction;
